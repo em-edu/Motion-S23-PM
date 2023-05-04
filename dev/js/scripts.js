74c7760e5bdf52373a882b5dcb4886adeea27c51
em-edu <81432745+em-edu@users.noreply.github.com>
2023-05-04 17:05:30 -0400

    ready

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,24 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
+
+
+// Init or Sets
+gsap.set("#hero h1 span",{alpha:0.25});
+
+
+function heroAnimation(){
+    var tl = gsap.timeline();
+    tl.from("#first-line",{duration:1,alpha:0, y:-100})
+    .from("#second-line",{duration:1,alpha:0, y:-100},"-=0.75")
+    .from("#trails-btn",{duration:1,y:100, alpha:0},"-=.5")
+    .from("#trails-btn i",{duration:0.5,rotation:90, alpha:0, transformOrigin: "left bottom"},"-=0.5");
+    return tl;
+}
+
+
+
+
+
+let mainTl = gsap.timeline();
+mainTl.add(heroAnimation())
+
+;
