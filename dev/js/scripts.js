3681c16f205d43587037cb52b30e06210a7c9d45
em-edu <81432745+em-edu@users.noreply.github.com>
2023-05-25 18:30:49 -0400

    sdfds

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,1 @@
 //import { gsap } from "gsap";
-import { GSDevTools } from "gsap/GSDevTools";
-import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-import { CustomEase } from "gsap/CustomEase";
