d98f4a077bd7a5c070a633f60a5342f462c3bf24
em-edu <81432745+em-edu@users.noreply.github.com>
2023-05-25 18:55:18 -0400

    added svg

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,14 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools);
+
+
+
+
+
+
+
+GSDevTools.create();
